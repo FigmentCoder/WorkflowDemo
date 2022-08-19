ff2b6af53d43339d3b3f5b52d7f2eb9b84355def
Michael Byrd <iamtron01@outlook.com>
2022-08-19 16:54:46 -0500

    housekeeping

--- WorkflowDemo.Common.Services/StringExtensions.cs
+++ WorkflowDemo.Common.Services/StringExtensions.cs
@@ -3,10 +3,10 @@ namespace WorkflowDemo.Common.Services
 {
     public static class StringExtensions
     {
-        public static string Concat(this string @this, string that)
-            => @this + that;
+        public static string Concat(this string left, string right)
+            => left + right;
 
-        public static string ConcatS(this string @this, string that)
-            => @this + " " + that;
+        public static string ConcatS(this string left, string right)
+            => left + " " + right;
     }
 }
